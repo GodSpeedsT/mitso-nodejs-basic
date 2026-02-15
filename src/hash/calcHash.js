e270b85dd32a8969e94a361800eb96e388502279
GodSpeedsT <167005914+GodSpeedsT@users.noreply.github.com>
2026-02-16 00:24:34 +0300

    hash func is done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import fs from 'fs/promises'
+import crypto from 'crypto'
+
 const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+    
+    try {
+        const data = await fs.readFile('./files/fileToCalculateHashFor.txt','utf-8');
+        console.log(data);
+        const hash = crypto.createHash('sha-256')
+        .update(data)
+        .digest('hex');
+
+        console.log(hash);
+    } catch(error) {
+        console.error("smth went wrong")
+    }
+
 };
 
 await calculateHash();
