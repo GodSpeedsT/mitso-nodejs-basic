ff9db200a5f619ad9f272a008c1ec0ff202be9ae
GodSpeedsT <167005914+GodSpeedsT@users.noreply.github.com>
2026-02-16 00:53:05 +0300

    streams done

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import fs from 'fs'
+
 const write = async () => {
     // Write your code here 
+
+const writable = fs.createWriteStream('./files/fileToWrite.txt');
+
+    process.stdin.pipe(writable);
+
+
 };
 
 await write();
