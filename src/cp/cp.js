46a8efa51141c4b8a54a12218936691e32c50d79
GodSpeedsT <167005914+GodSpeedsT@users.noreply.github.com>
2026-02-16 12:02:40 +0300

    cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,19 @@
+import { fileURLToPath } from "url";
+import { fork } from "child_process";
+import { cpus } from "os";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const scriptFiles = path.join(__dirname, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  // Write your code here
+
+  fork(scriptFiles, args, {
+    stdio: [process.stdin, process.stdout, "ipc"],
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(/* [someArgument1, someArgument2, ...] */);
